92fbc77c40f1fc24dbabd2832fe2c5996a26ec9e
giorgizosidze <zosidzegiorgi3@gmail.com>
2025-05-04 01:01:02 +0400

    second commit

---
+++ day 03/homework.py
@@ -0,0 +1,82 @@
+print("Hello world!")
+
+name = "giorgi"
+surname = "zosidze"
+age = 15
+
+print(name)
+print(surname)
+print(age)
+ 
+
+("Hello my name is giorgi Iam 15 years old My surname is zosidze")
+print("Hello My name is giorgi iam 15 years old My surname is zosidze")
+
+
+num1 = 14
+num2 = 20
+num3 = 10
+num4 = 30
+num5 = 40
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+# mimateba
+
+
+num1 + num2 = 34
+num2 + num3 = 30
+num3 + num4 = 40
+num4 + num5 = 70
+
+print(num1 + num2)
+print(num2 + num3)
+print(num3 + num4)
+print(num4 + num5)
+
+
+
+
+# gamokleba
+
+num1 - num2 = -6
+num2 - num3 = 10
+num3 - num4 = -20
+num4 - num5 =-10
+
+print(num1 - num2)
+print(num2 -num3)
+print(num3 - num4)
+print(num4 - num5)
+
+# gamravleba
+
+num1 * num2 = 280
+num2 * num3 = 200
+num3 * num4 = 300
+num4 * num5 = 1200
+
+print(num1 * num2)
+print(num2 * num3)
+print(num3 * num4)
+print(num4 * num5)
+
+
+# gayofa
+
+num1 / num2 = 0.7
+num2 / num3 = 2
+num3 / num4 = 0.3
+num4 / num5 = 0.75
+
+print(num1 / num2)
+print(num2 / num3)
+print(num3 / num4)
+print(num4 / num5)
+
+
+
