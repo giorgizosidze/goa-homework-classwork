d7b4c852fa89e873118fe9f1911e348bf8dbff8f
giorgizosidze <zosidzegiorgi3@gmail.com>
2025-05-09 00:02:09 +0400

    second commit

---
+++ day 04/classwork/classwork.py
@@ -0,0 +1,12 @@
+print("hello world!")
+
+
+name = "giorgi"
+surname = "zosidze"
+age = "15"
+age2 = "13"
+hobby = "football"
+
+
+print(f"hello my name is {name} my age is {age} my surname is {surname} my sister age is {age2} and my favorite hobby is {hobby}")
+
