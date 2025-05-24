23cd0e30a65e6313cd7e6700e611b5909c9859f1
giorgizosidze <zosidzegiorgi3@gmail.com>
2025-05-24 19:05:00 +0400

    second commit

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,27 @@
+print("Hello world!")
+
+
+int1 = int(input("start: "))
+int2 = int(input("step: "))
+int3 = int(input("end: "))
+
+   
+for i in range(int1, int2, int3("num: ")):
+    i = 0
+    while i!= int1:
+     print(i)
+    i + 1
+
+
+int = int(input)
+
+i = 0
+while i!= int1:
+    print(i)
+    i += 1
+
+
+num = int(input("num: "))
+
+for i in range(num):
+    print(i)
