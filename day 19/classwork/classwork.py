966b2a932d7a4356d55b6baa79979f170921d70d
giorgizosidze <zosidzegiorgi3@gmail.com>
2025-06-30 20:51:37 +0400

    second commit

---
+++ day 19/classwork/classwork.py
@@ -0,0 +1,15 @@
+# 1 davaleba
+
+# indeqsi gvexmareba rom davnomrot mnisvnelobebi cvladsi
+
+
+# 2 davaleba
+
+
+phone = ["iphone", "redmi", "samsung", "realmi" ]
+
+phone[1] ="poco" 
+phone[2] ="iphone 13"
+phone[3] = "iphone 20"
+phone[4] = "samsung c 5"
+phone[5] = "iphone 69"
---
+++ day 19/homework/homework.py
@@ -0,0 +1,38 @@
+# 1 davaleba
+
+number = [10, 20, 30, 40, 50, 60, 70]
+
+print(number[0 : 3])
+
+
+# 2 davaleba
+
+
+letters = ["a", "b", "c", "d", "e", "f"]
+
+
+print(letters[2 : 5])
+
+
+# 3 davaleba
+
+
+words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+
+print(words[4 : 6])
+
+
+# 4 davaleba
+
+
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+
+print(colors[2 : 5])
+
+
+# 5 davaleba
+
+
+text = "programming"
+
+print(text[0 : 5])
